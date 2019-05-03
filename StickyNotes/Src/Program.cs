c4477f7f643b1c5ec3cb652f0249957e8f86a3ae
Ramon Mendes <ramon@misoftware.com.br>
2019-05-03 19:26:15 -0300

    first commit

---
+++ StickyNotes/Src/Program.cs
@@ -0,0 +1,54 @@
+using SciterSharp;
+using SciterSharp.Interop;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StickyNotes
+{
+	class Program
+	{
+		public static Window WndGlobal;
+		public static Hooker HookerInstance = new Hooker();
+
+		[STAThread]
+		static void Main(string[] args)
+		{
+			// Sciter needs this for drag'n'drop support; STAThread is required for OleInitialize succeess
+			int oleres = PInvokeWindows.OleInitialize(IntPtr.Zero);
+			Debug.Assert(oleres == 0);
+			
+			// Create the window
+			var wnd = WndGlobal = new Window();
+			wnd.CreateMainWindow(500, 320,
+				SciterXDef.SCITER_CREATE_WINDOW_FLAGS.SW_MAIN |
+				SciterXDef.SCITER_CREATE_WINDOW_FLAGS.SW_ALPHA |
+				SciterXDef.SCITER_CREATE_WINDOW_FLAGS.SW_POPUP |
+				SciterXDef.SCITER_CREATE_WINDOW_FLAGS.SW_ENABLE_DEBUG);
+			wnd.CenterTopLevelWindow();
+#if !DEBUG
+			wnd.HideTaskbarIcon();
+#endif
+			wnd.Title = "Sciter-based desktop widget RLZ";
+			wnd.Icon = Properties.Resources.IconMain;
+
+			// Prepares SciterHost and then load the page
+			var host = new Host();
+			host.Setup(wnd);
+			host.AttachEvh(new HostEvh());
+			host.SetupPage("index.html");
+
+			HookerInstance.SetMessageHook();
+
+			// Show window and Run message loop
+			wnd.Show();
+			PInvokeUtils.RunMsgLoop();
+
+			HookerInstance.ClearHook();
+		}
+	}
+}
