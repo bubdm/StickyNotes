344ad4035ec6ebae951e7ec3d6df82c10b4583b8
Ramon Mendes <rfmendesbrazil@gmail.com>
2021-11-03 10:54:00 -0300

    Sciter.js migration progress

--- StickyNotes/res/js/settings.js
+++ StickyNotes/res/js/settings.js
@@ -16,14 +16,21 @@ class Settings {
 	static root;
 
 	static setup() {
-		let read = Window.this.xcall("Host_ReadFile", PATH);
-		let data = eval('(' + read + ')') || initDb();
-		Settings.root = data;
-		console.log(JSON.stringify(Settings.root));
+		if(globalThis.settings_root) {
+			Settings.root = globalThis.settings_root;
+			console.log('globalThis.settings_root');
+		}
+		else {
+			console.log('else');
+			let read = Window.this.xcall("Host_ReadFile", PATH);
+			let data = eval('(' + read + ')') || initDb();
+			Settings.root = data;
+			globalThis.settings_root = data;
+			console.log(globalThis.settings_root);
+		}
 	}
 
 	static commit() {
-		console.log(JSON.stringify(Settings.root));
 		Window.this.xcall("Host_WriteFile", PATH, JSON.stringify(Settings.root))
 	}
 }
